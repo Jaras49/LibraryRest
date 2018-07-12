b8df366f5ce48a1e2ae5e374d8c8bfac8a0c0eb3
Jaras49 <jaras48@gmail.com>
2018-07-12 19:41:08 +0200

    RentRepositoryTestSuite added tests

--- src/test/java/com/library/repository/rent/RentRepositoryTestSuite.java
+++ src/test/java/com/library/repository/rent/RentRepositoryTestSuite.java
@@ -5,12 +5,9 @@ import com.library.domain.rent.Rent;
 import com.library.domain.user.User;
 import com.library.exception.RentException;
 import com.library.repository.book.BookDao;
-import com.library.repository.title.TitleDao;
 import com.library.repository.user.UserDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -19,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.stream.LongStream;
 
 import static org.junit.Assert.*;
 
@@ -28,8 +26,6 @@ import static org.junit.Assert.*;
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql.sql")
 public class RentRepositoryTestSuite {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RentRepositoryTestSuite.class);
-
     @Autowired
     private UserDao userDao;
     @Autowired
@@ -37,27 +33,12 @@ public class RentRepositoryTestSuite {
     @Autowired
     private RentDao rentDao;
     @Autowired
-    private TitleDao titleDao;
-    @Autowired
     private RentRepository rentRepository;
 
-    //TODO
     @Test
     public void shouldRentBooks() throws RentException {
 
-
-        rentDao.findRents().forEach(System.out::println);
-
-
-        assertEquals(0, userDao.findById(3L).get().getRents().size());
-        assertEquals(2, rentDao.findAll().size());
-        assertEquals(1, userDao.findById(2L).get().getRents().size());
-        assertEquals(1, userDao.findById(1L).get().getRents().size());
-        assertEquals(2, rentDao.findAll().size());
-        assertEquals(3, userDao.findAll().size());
-        assertEquals(2, titleDao.findAll().size());
-        assertEquals(11, bookDao.findAll().size());
-
+        //When
         rentRepository.rentBooks(3L, 7L, 15L);
 
         //Then
@@ -66,7 +47,7 @@ public class RentRepositoryTestSuite {
         assertEquals(3, rentDao.findAll().size());
 
         Book book = bookDao.findById(15L).get();
-        assertEquals(Book.Status.RENTED,  book.getStatus());
+        assertEquals(Book.Status.RENTED, book.getStatus());
 
         assertEquals(Book.Status.RENTED, bookDao.findById(7L).get().getStatus());
 
@@ -77,10 +58,64 @@ public class RentRepositoryTestSuite {
         assertEquals(2, rents.size());
         Book.@NotNull Status status = rents.get(0).getRentedBooks().get(0).getStatus();
         assertEquals(Book.Status.RENTED, status);
-        assertEquals(2, rents.size());
         assertEquals(2, rents.get(0).getRentedBooks().size());
-        assertEquals(2 , rents.get(1).getRentedBooks().size());
-
+        assertEquals(2, rents.get(1).getRentedBooks().size());
     }
 
+    @Test
+    public void shouldReturnRent() throws RentException {
+
+        //When
+        rentRepository.returnRent(33L);
+
+        //Then
+        assertEquals(1, rentDao.findAll().size());
+        assertFalse(rentDao.findById(33L).isPresent());
+
+        assertEquals(0, userDao.findById(1L).get().getRents().size());
+
+        LongStream.of(8, 9, 11, 12)
+                .mapToObj(value -> bookDao.findById(value).get())
+                .forEach(book -> {
+                    assertNull(book.getRent());
+                    assertEquals(Book.Status.AVAILABLE, book.getStatus());
+                });
+    }
+
+    @Test
+    public void shouldReturnBooks() {
+
+        //When
+        rentRepository.returnBooks(12L, 13L);
+
+        //Then
+        assertEquals(1, rentDao.findAll().size());
+        assertEquals(3, rentDao.findById(33L).get().getRentedBooks().size());
+
+        LongStream.of(12, 13)
+                .mapToObj(value -> bookDao.findById(value).get())
+                .forEach(book -> {
+                    assertNull(book.getRent());
+                    assertEquals(Book.Status.AVAILABLE, book.getStatus());
+                });
+    }
+
+    @Test
+    public void shouldThrowRentException() {
+
+        try {
+            rentRepository.rentBooks(100L, 5L, 6L);
+        } catch (RentException e) {
+            assertEquals(RentException.INVALID_USER_ID, e.getMessage());
+        }
+        try {
+            rentRepository.rentBooks(1L, 100L, 150L);
+        } catch (RentException e) {
+            assertEquals(RentException.NOTHING_RENTED, e.getMessage());
+        }try {
+            rentRepository.returnRent(100L);
+        } catch (RentException e) {
+            assertEquals(RentException.INVALID_RENT_ID, e.getMessage());
+        }
+    }
 }
