b082f2cfcc326d2c5fb5dd7aebbc8c8e467db315
Jaras49 <jaras48@gmail.com>
2018-07-14 22:27:15 +0200

    TitleMapper added mapToTitleDtoList method

--- src/main/java/com/library/mapper/TitleMapper.java
+++ src/main/java/com/library/mapper/TitleMapper.java
@@ -4,11 +4,26 @@ import com.library.domain.title.Title;
 import com.library.domain.title.TitleDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 @Component
 public class TitleMapper {
 
     public Title mapToTitle (TitleDto titleDto) {
 
         return new Title(titleDto.getTitle(), titleDto.getAuthor(), titleDto.getYearOfPublication());
+    }
+
+    public List<TitleDto> mapToTitleDtoList(List<Title> titles) {
+
+        return titles.stream()
+                .map(title -> new TitleDto(
+                        title.getId(),
+                        title.getTitleName(),
+                        title.getAuthor(),
+                        title.getYearOfPublication()))
+                .collect(toList());
     }
 }
