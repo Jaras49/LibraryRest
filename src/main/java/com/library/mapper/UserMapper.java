83f5f780591861528633f01d289a6d3ffa7e00c1
Jaras49 <jaras48@gmail.com>
2018-07-14 22:46:10 +0200

    added mapToUserDtoList

--- src/main/java/com/library/mapper/UserMapper.java
+++ src/main/java/com/library/mapper/UserMapper.java
@@ -4,11 +4,25 @@ import com.library.domain.user.User;
 import com.library.domain.user.UserDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 @Component
 public class UserMapper {
 
     public User mapToUser(UserDto userDto) {
 
-        return new User (userDto.getFirstName(), userDto.getLastName());
+        return new User(userDto.getFirstName(), userDto.getLastName());
+    }
+
+    public List<UserDto> mapToUserDtoList(List<User> users) {
+
+        return users.stream()
+                .map(user -> new UserDto(
+                        user.getId(),
+                        user.getFirstName(),
+                        user.getLastName()))
+                .collect(toList());
     }
 }
